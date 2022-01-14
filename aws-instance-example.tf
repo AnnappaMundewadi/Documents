f85fb008ba893f685e31e0321d4e29db99f0d7a9
AnnappaMundewadi <97218686+AnnappaMundewadi@users.noreply.github.com>
2022-01-14 13:41:14 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web1" {
-   ami           = "ami-04ad2567c9e3d7893"
+   ami           = "ami-08e4e35cccc6189f4"
    instance_type = "t2.micro"
    count = 1
-  vpc_security_group_ids = ["sg-050c66b16eccd3620"]
+  vpc_security_group_ids = ["sg-03fb74a83e964e06f"]
    key_name               = "Linux_Terraform-Chef" 
    iam_instance_profile =   "myManagedInstanceRole"
    user_data = <<-EOF
@@ -10,9 +10,9 @@ resource "aws_instance" "web1" {
       sudo amazon-linux-extras install ansible2 -y
       sudo yum install git -y
       sudo yum install maven -y      
-      git -C ./home/ec2-user clone https://github.com/GarvitKushwaha/Terra-Ansi-jFrog.git          
+      git -C ./home/ec2-user clone https://github.com/AnnappaMundewadi/Documents.git          
       cd /home/ec2-user/Terra-Ansi-jFrog && ansible-playbook main.yml -f 10
-      cd /home/ec2-user/Terra-Ansi-jFrog && wget -O Java-Ansible.war https://abishek.jfrog.io/artifactory/abi_new/MyMavanWebapp/Java-Ansible/0.0.1-SNAPSHOT/Java-Ansible-0.0.1-SNAPSHOT.war
+      cd /home/ec2-user/Terra-Ansi-jFrog && wget -O Java-Ansible.war https://annappa.jfrog.io/artifactory/annappa_new/MyMavanWebapp/Java-Ansible/0.0.1-SNAPSHOT/Java-Ansible-0.0.1-SNAPSHOT.war
       cd /home/ec2-user/Terra-Ansi-jFrog && ansible-playbook deploy.yml -f 10
       EOF
    }
